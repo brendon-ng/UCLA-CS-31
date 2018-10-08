756f77a989cb36a63e51a44bfa2d1be18025f1dd
brendon-ng <brendonn8@gmail.com>
2018-10-08 10:56:16 -0700

    Add Project 1

---
+++ Project1/original.cpp
@@ -0,0 +1,34 @@
+// Code for Project 1
+// Report poll results
+
+#include <iostream>
+using namespace std;   // pp. 38-39 in Savitch 6/e explains this line
+
+int main()
+{
+    int numberSurveyed;
+    int forNewsom;
+    int forCox;
+    
+    cout << "How many registered voters were surveyed? ";
+    cin >> numberSurveyed;
+    cout << "How many of them say they will vote for Newsom? ";
+    cin >> forNewsom;
+    cout << "How many of them say they will vote for Cox? ";
+    cin >> forCox;
+    
+    double pctNewsom = 100.0 * forNewsom / numberSurveyed;
+    double pctCox = 100.0 * forCox / numberSurveyed;
+    
+    cout.setf(ios::fixed);       // see pp. 32-33 in Savitch 6/e
+    cout.precision(1);
+    
+    cout << endl;
+    cout << pctNewsom << "% say they will vote for Newsom." << endl;
+    cout << pctCox << "% say they will vote for Cox." << endl;
+    
+    if (forNewsom > forCox)
+        cout << "Newsom is predicted to win the election." << endl;
+    else
+        cout << "Cox is predicted to win the election." << endl;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Stats 20 Labs and Exams
+#### Brendon Ng
+#### Stats 20: Statistical Programming with R
+#### UCLA Spring 2018
+#### Prof. Vivian Lew
+Included are all lab assignments and laboratory exams from Stats 20 taken during the Spring of 2018. Labs were written in R Markdown files and the final visual lab write up is saved in a HTML files. <br/><br/>
+
+To see final products for lab assignments and exams, download the repo and open the corresponding HTML file. <br/><br/>
+
+Topics include graphics, visualizing data, manipulating DataFrames, extracting data, drawing conclusions from datasets, and more.
+
