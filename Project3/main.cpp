9526b1716987d59a4239d13395994e8468791401
brendon-ng <brendonn8@gmail.com>
2018-10-22 12:56:53 -0700

    Project 3 initial commit

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Brendon Ng on 10/22/18.
+//  Copyright © 2018 Brendon Ng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
