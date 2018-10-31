85acf7685ee75e091713ca77b632b85c322e7fe7
brendon-ng <brendonn8@gmail.com>
2018-10-31 12:23:12 -0700

    initial commit

---
+++ Project4/array.cpp
@@ -0,0 +1,15 @@
+//
+//  array.cpp
+//  Project4
+//
+//  Created by Brendon Ng on 10/31/18.
+//  Copyright © 2018 Brendon Ng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
