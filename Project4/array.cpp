3554d8c7feb9149443ef3292cb2117ddf5f37c24
brendon-ng <brendonn8@gmail.com>
2018-11-02 20:00:45 -0700

    First half of functions

--- Project4/array.cpp
+++ Project4/array.cpp
@@ -7,9 +7,218 @@
 //
 
 #include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+int appendToAll(string a[], int n, string value) {
+    if(n < 0)
+        return -1;
+    for(int i=0; i < n; i++){
+        a[i] =  a[i] + value;
+    }
+    return n;
+}
+
+int lookup(const string a[], int n, string target) {
+    if(n < 0)
+        return -1;
+    for(int i=0; i < n; i++){
+        if(a[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+int positionOfMax(const string a[], int n){
+    if(n <= 0)
+        return -1;
+    string max = "";
+    int index = 0;
+    for(int i=0; i < n; i++){
+        if(a[i] > max){
+            index = i;
+            max = a[i];
+        }
+    }
+    return index;
+}
+
+int rotateLeft(string a[], int n, int pos){
+    if (n < 0 || pos > n)
+        return -1;
+    string move = a[pos];
+    for(int i=pos; i < n-1; i++){
+        a[i] = a[i+1];
+    }
+    a[n-1] = move;
+    return pos;
+}
+
+int countRuns(const string a[], int n){
+    if(n < 0)
+        return -1;
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    int count = 1;
+    for(int i=1; i < n; i++){
+        if(a[i] != a[i-1])
+            count++;
+    }
+    return count;
+}
+
+int flip(string a[], int n){
+    if(n < 0)
+        return -1;
+    for(int i=0; i<n/2; i++){
+        string temp = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = temp;
+    }
+    return n;
+}
+
+int differ(const string a1[], int n1, const string a2[], int n2){
+    if(n1 < 0 || n2 < 0)
+        return -1;
+    for(int i=0; i < min(n1, n2); i++){
+        if(a1[i] != a2[i])
+            return i;
+    }
+    return min(n1,n2);
+}
+
+int subsequence(const string a1[], int n1, const string a2[], int n2){
+    if(n1 < 0 || n2 < 0)
+        return -1;
+    if(n2 ==0)
+        return 0;
+    if(n1 == 0)
+        return -1;
+    for(int i=0; i < n1-n2; i++){
+        if(a1[i]==a2[0]){
+            for(int j=i+1, k=1; j < i+n2; j++, k++){
+                if(a1[j] != a2[k])
+                    continue;
+            }
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    //Test cases for appendToAll()
+    string people[5] = { "dianne", "fiona", "ed", "xavier", "greg" };
+    int j = appendToAll(people, 5, "!!!");  // returns 5
+    assert(j==5 && people[0] == "dianne!!!" && people[4] == "greg!!!" && people[1] == "fiona!!!");
+    
+    string peopletwo[5] = { "dianne", "fiona", "ed", "xavier", "greg" };
+    j = appendToAll(peopletwo, 0, "!!!");
+    assert(j == 0 && peopletwo[0] == "dianne" && peopletwo[1] == "fiona" && peopletwo[4] == "greg");
+    
+    j = appendToAll(peopletwo, -4, "!!!");
+    assert(j == -1 && peopletwo[0] == "dianne" && peopletwo[1] == "fiona" && peopletwo[4] == "greg");
+    
+    j = appendToAll(peopletwo, 2, "!!!");
+    assert(j == 2 && peopletwo[0] == "dianne!!!" && peopletwo[1] == "fiona!!!" && peopletwo[4] == "greg");
+    
+    cout << "Tests for appendToAll() passed!" << endl;
+    
+    //Test cases for lookup()
+    string lookupStr[6] = { "dianne", "fiona", "ed", "xavier", "greg", "ed"};
+    assert(lookup(lookupStr, 5, "fiona") == 1);
+    assert(lookup(lookupStr, -1, "fiona") == -1);
+    assert(lookup(lookupStr, 6, "ed") == 2);
+    assert(lookup(lookupStr, 5, "Xavier") == -1);
+    assert(lookup(lookupStr, 0, "fiona") == -1);
+    assert(lookup(lookupStr, 5, "dianne") == 0);
+    assert(lookup(lookupStr, 1, "dianne") == 0);
+    cout << "Tests for lookup() passed!" << endl;
+    
+    //Test cases for positionOfMax()
+    string candidate[7] = { "dianne", "fiona", "gavin", "xavier", "ed", "betty", "xavier"};
+    assert(positionOfMax(candidate, 6) == 3);
+    assert(positionOfMax(candidate, 7) == 3);
+    assert(positionOfMax(candidate, 0) == -1);
+    assert(positionOfMax(candidate, -1) == -1);
+    cout << "Tests for positionOfMax() passed!" << endl;
+    //Need More
+    
+    //Test cases for rotateLeft()
+    string politician[5] = { "eleni", "dianne", "fiona", "kevin", "gavin" };
+    assert(rotateLeft(politician, 5, 1) == 1 && politician[0] == "eleni" && politician[1] == "fiona" && politician[2] == "kevin" && politician[3] == "gavin" && politician[4] == "dianne");
+    string testRotate[5] = { "eleni", "dianne", "fiona", "kevin", "gavin" };
+    assert(rotateLeft(testRotate, 5, 3) == 3 && testRotate[0] == "eleni" && testRotate[1] == "dianne" && testRotate[2] == "fiona" && testRotate[3] == "gavin" && testRotate[4] == "kevin");
+    assert(rotateLeft(testRotate, 3, 1) == 1 && testRotate[0] == "eleni" && testRotate[1] == "fiona" && testRotate[2] == "dianne" && testRotate[3] == "gavin" && testRotate[4] == "kevin");
+    assert(rotateLeft(testRotate, -1, -4) == -1);
+    assert(rotateLeft(testRotate, 2, 4) == -1);
+    cout << "Tests for rotateLeft() passed!" << endl;
+    
+    //Test cases for countRuns()
+    string d[9] = {"xavier", "betty", "john", "john", "ed", "ed", "ed", "john", "john"};
+    assert(countRuns(d, 9) == 5);
+    assert(countRuns(d, 1) == 1);
+    assert(countRuns(d, 2) == 2);
+    assert(countRuns(d, 4) == 3);
+    assert(countRuns(d, 0) == 0);
+    assert(countRuns(d, -1) == -1);
+    cout << "Tests for countRuns() passed!" << endl;
+    
+    //Test cases for flip()
+    string folks[6] = { "betty", "john", "", "xavier", "kevin", "dianne" };
+    assert(flip(folks, 4)==4 && folks[0]=="xavier" && folks[1]=="" && folks[2]=="john" && folks[3]=="betty" && folks[4]=="kevin" && folks[5]=="dianne");
+    assert(flip(folks, 1)==1 && folks[0]=="xavier" && folks[1]=="" && folks[2]=="john" && folks[3]=="betty" && folks[4]=="kevin" && folks[5]=="dianne");
+    assert(flip(folks, 0)==0 && folks[0]=="xavier" && folks[1]=="" && folks[2]=="john" && folks[3]=="betty" && folks[4]=="kevin" && folks[5]=="dianne");
+    assert(flip(folks, -1)==-1 && folks[0]=="xavier" && folks[1]=="" && folks[2]=="john" && folks[3]=="betty" && folks[4]=="kevin" && folks[5]=="dianne");
+    assert(flip(folks, 5)==5 && folks[0]=="kevin" && folks[1]=="betty" && folks[2]=="john" && folks[3]=="" && folks[4]=="xavier" && folks[5]=="dianne");
+    assert(flip(folks, 6)==6 && folks[0]=="dianne" && folks[1]=="xavier" && folks[2]=="" && folks[3]=="john" && folks[4]=="betty" && folks[5]=="kevin");
+    string t[7] = { "greg", "gavin", "ed", "xavier", "", "eleni", "fiona" };
+    assert(flip(t, 7)==7 && t[0]=="fiona" && t[1]=="eleni" && t[2]=="" && t[3]=="xavier" && t[4]=="ed" && t[5]=="gavin" && t[6]=="greg");
+    cout << "Tests for flip() passed!" << endl;
+    
+    //Test cases for differ()
+    string folks2[6] = { "betty", "john", "", "xavier", "kevin", "dianne" };
+    string group[5] = { "betty", "john", "dianne", "", "xavier" };
+    assert(differ(folks2, 6, group, 5) == 2);
+    assert(differ(folks2, 2, group, 1) == 1);
+    assert(differ(folks2, 0, group, 1) == 0);
+    assert(differ(folks2, 3, group, 6) == 2);
+    assert(differ(folks2, 2, group, 6) == 2);
+    string str1[6] = { "betty", "john", "", "xavier", "kevin", "dianne" };
+    string str2[5] = { "john", "john", "dianne", "", "xavier" };
+    assert(differ(str1, 2, str2, 6) == 0);
+    string str3[6] = { "betty", "john", "", "xavier", "kevin", "dianne" };
+    string str4[5] = { "betty", "john", "", "xavier", "kevin" };
+    assert(differ(str3, 6, str4, 5) == 5);
+    cout << "Tests for differ() passed!" << endl;
+    
+    //Test cases for subsequence()
+    string names[10] = { "eleni", "gavin", "kevin", "greg", "betty", "fiona" };
+    string names1[10] = { "gavin", "kevin", "greg" };
+    assert(subsequence(names, 6, names1, 3) == 1);
+    string names2[10] = { "eleni", "greg" };
+    assert(subsequence(names, 5, names2, 2) == -1);
+
+    
+    
+    
+    
+    
+    
+    string h[7] = { "greg", "gavin", "ed", "xavier", "", "eleni", "fiona" };
+    assert(lookup(h, 7, "eleni") == 5);
+    assert(lookup(h, 7, "ed") == 2);
+    assert(lookup(h, 2, "ed") == -1);
+    assert(positionOfMax(h, 7) == 3);
+    
+   
+    
+    
+    
+    cout << "All Tests Passed!" << endl;
 }
