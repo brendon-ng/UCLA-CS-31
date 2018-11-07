0723a446724b084ec368598a0d08611ec6076d3c
brendon-ng <brendonn8@gmail.com>
2018-11-07 12:07:21 -0800

    Initial commit Project 5

---
+++ Project5/rate.cpp
@@ -0,0 +1,15 @@
+//
+//  rate.cpp
+//  Project5
+//
+//  Created by Brendon Ng on 11/7/18.
+//  Copyright © 2018 Brendon Ng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
